1f129ba17682575e6dc55c85f269f408411f8c4d
Donna Adamo <donnastefania.adamo@mercadolibre.com>
2022-04-27 18:20:17 -0300

    initial setup and header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Header />
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
