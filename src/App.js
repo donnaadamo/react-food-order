3719d6078014596d57f9f85f45cd64b04d5c0de1
Donna Adamo <donnastefania.adamo@mercadolibre.com>
2022-04-27 22:39:31 -0300

    items and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <div>
       <Fragment>
         <Header />
+        <main>
+          <Meals />
+        </main>
       </Fragment>
     </div>
   );
